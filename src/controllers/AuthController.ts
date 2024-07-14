87b79fb4f27309152598a721fe1afd6fdf90f932
Renan Leite <renan.vbleite@gmail.com>
2024-07-13 21:35:35 -0300

    Removing secret from source code

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,16 +2,27 @@ import jwt from 'jsonwebtoken'
 import User from '../models/UserModel'
 import { Request, Response } from 'express'
 import bcrypt from 'bcryptjs'
+import dotenv from 'dotenv'
 
-const secret = 'your_jwt_secret'
+dotenv.config()
+
+const { SECRET } = process.env
+
+if (!SECRET) {
+    throw new Error('JWT Secret not set')
+}
 
 export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body
 
+    if(!email || !password) {
+        res.json({ error: 'Credentials missing'})
+    }
+
     const user = await User.findOne({ where: { email } })
 
     if (user && (await bcrypt.compare(password, user.password))) {
-        const token = jwt.sign({ userId: user.user_id }, secret, { expiresIn: '1h' })
+        const token = jwt.sign({ userId: user.user_id }, SECRET, { expiresIn: '1h' })
         res.json({ token })
     } else {
         res.status(401).json({ error: 'Invalid credentials' })
@@ -25,7 +36,7 @@ export const protect = (req: Request, res: Response, next: Function) => {
         return res.status(401).json({ error: 'No token provided' })
     }
 
-    jwt.verify(token, secret, (err: any) => {
+    jwt.verify(token, SECRET, (err: any) => {
         if (err) {
             return res.status(401).json({ error: 'Invalid token' })
         }
